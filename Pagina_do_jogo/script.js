891a2d81c2c8001c16282d8a8032b0e464db687a
Lucas Piancó <lucaspianco133@gmail.com>
2026-06-16 18:44:20 -0300

    Arrumando as imgs de funndo, titulo do index, tipografia

--- Pagina_do_jogo/script.js
+++ Pagina_do_jogo/script.js
@@ -20,6 +20,17 @@ for (let i = 0; i < 10; i++) {
 for (let i = 0; i < 10; i++) {
   galeria.push('img/Ship-3.png');
 }
+function embaralhar(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+embaralhar(galeria);
+
 
 document.addEventListener('DOMContentLoaded', () => {
   criartabela();
